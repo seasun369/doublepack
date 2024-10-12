b2c2ace18b23d1c65ba592a7c986b2f220e67c1e
seasun369 <seasun@sjtu.edu.cn>
2024-10-12 16:39:12 +0800

    offline

--- src/dp/circuits/offline.cc
+++ src/dp/circuits/offline.cc
@@ -7,7 +7,7 @@ namespace dp {
     }
     for (std::size_t layer = 0; layer < GetDepth(); layer++) {
       for (auto mult_gate : mFlatMultLayers[layer]) {
-	mult_gate->SetDummyLambda(lambda);
+	      mult_gate->SetDummyLambda(lambda);
       }
     }
     for (auto output_gate : mOutputGates) {
@@ -17,55 +17,57 @@ namespace dp {
     // Now update packed sharings
     for (auto input_layer : mInputLayers) {
       for (auto input_batch : input_layer.mBatches) {
-	Vec lambda_A;
-	lambda_A.Reserve(mBatchSize);
-	for (std::size_t i = 0; i < mBatchSize; i++) {
-	  lambda_A.Emplace(input_batch->GetInputGate(i)->GetDummyLambda());
-	}
-	scl::PRG prg;
-	auto poly = scl::details::EvPolyFromSecretsAndDegree(lambda_A, mBatchSize-1, prg);
-	Vec new_shares = scl::details::SharesFromEvPoly(poly, mParties);
+	      Vec lambda_A;
+	      lambda_A.reserve(mBatch_m);
+	      for (std::size_t i = 0; i < mBatch_m; i++) {
+	        lambda_A.emplace_back(input_batch->GetInputGate(i)->GetDummyLambda());
+	      }
+	      //scl::PRG prg;
+	      //auto poly = scl::details::EvPolyFromSecretsAndDegree(lambda_A, mBatchSize-1, prg);
+	      //Vec new_shares = scl::details::SharesFromEvPoly(poly, mParties);
 
-	input_batch->SetPreprocessing(new_shares[mID]);
+        vec_ZZ_pE new_shares = Scheme_m1.create_shares(lambda_A);
+
+	      input_batch->SetPreprocessing(new_shares[mID]);
       }
     }
     for (auto output_layer : mOutputLayers) {
       for (auto output_batch : output_layer.mBatches) {
-	Vec lambda_A;
-	lambda_A.Reserve(mBatchSize);
-	for (std::size_t i = 0; i < mBatchSize; i++) {
-	  lambda_A.Emplace(output_batch->GetOutputGate(i)->GetDummyLambda());
-	}
-	scl::PRG prg;
-	auto poly = scl::details::EvPolyFromSecretsAndDegree(lambda_A, mBatchSize-1, prg);
-	Vec new_shares = scl::details::SharesFromEvPoly(poly, mParties);
-
-	output_batch->SetPreprocessing(new_shares[mID]);
+	      Vec lambda_A;
+	      lambda_A.reserve(mBatch_m);
+	      for (std::size_t i = 0; i < mBatch_m; i++) {
+	        lambda_A.emplace_back(output_batch->GetOutputGate(i)->GetDummyLambda());
+	      }
+	      //scl::PRG prg;
+	      //auto poly = scl::details::EvPolyFromSecretsAndDegree(lambda_A, mBatchSize-1, prg);
+	      //Vec new_shares = scl::details::SharesFromEvPoly(poly, mParties);
+        vec_ZZ_pE new_shares = Scheme_m1.create_shares(lambda_A);
+	      output_batch->SetPreprocessing(new_shares[mID]);
       }
     }
     // Mults
     for (auto mult_layer : mMultLayers) {
       for (auto mult_batch : mult_layer.mBatches) {
-	Vec lambda_A;
-	Vec lambda_B;
-	Vec lambda_C;
-	lambda_A.Reserve(mBatchSize);
-	lambda_B.Reserve(mBatchSize);
-	lambda_C.Reserve(mBatchSize);
-	for (std::size_t i = 0; i < mBatchSize; i++) {
-	  lambda_A.Emplace(mult_batch->GetMultGate(i)->GetLeft()->GetDummyLambda());
-	  lambda_B.Emplace(mult_batch->GetMultGate(i)->GetRight()->GetDummyLambda());
-	  lambda_C.Emplace(mult_batch->GetMultGate(i)->GetDummyLambda());
-	}
-	scl::PRG prg;
-	auto poly_A = scl::details::EvPolyFromSecretsAndDegree(lambda_A, mBatchSize-1, prg);
-	auto poly_B = scl::details::EvPolyFromSecretsAndDegree(lambda_B, mBatchSize-1, prg);
-	auto poly_C = scl::details::EvPolyFromSecretsAndDegree(lambda_C, mBatchSize-1, prg);
-	Vec new_shares_A = scl::details::SharesFromEvPoly(poly_A, mParties);
-	Vec new_shares_B = scl::details::SharesFromEvPoly(poly_B, mParties);
-	Vec new_shares_C = scl::details::SharesFromEvPoly(poly_C, mParties);
+	      Vec lambda_A;
+	      Vec lambda_B;
+	      Vec lambda_C;
+	      lambda_A.reserve(mBatch_m);
+	      lambda_B.reserve(mBatch_m);
+	      lambda_C.reserve(mBatch_m);
+	      for (std::size_t i = 0; i < mBatch_m; i++) {
+	        lambda_A.emplace_back(mult_batch->GetMultGate(i)->GetLeft()->GetDummyLambda());
+	        lambda_B.emplace_back(mult_batch->GetMultGate(i)->GetRight()->GetDummyLambda());
+	        lambda_C.emplace_back(mult_batch->GetMultGate(i)->GetDummyLambda());
+	      }
+	      //scl::PRG prg;
+	      //auto poly_A = scl::details::EvPolyFromSecretsAndDegree(lambda_A, mBatchSize-1, prg);
+	      //auto poly_B = scl::details::EvPolyFromSecretsAndDegree(lambda_B, mBatchSize-1, prg);
+	      //auto poly_C = scl::details::EvPolyFromSecretsAndDegree(lambda_C, mBatchSize-1, prg);
+	      vec_ZZ_pE new_shares_A = Scheme_m1.create_shares(lambda_A);
+	      vec_ZZ_pE new_shares_B = Scheme_m1.create_shares(lambda_B);
+	      vec_ZZ_pE new_shares_C = Scheme_m1.create_shares(lambda_C);
 
-	mult_batch->SetPreprocessing(new_shares_A[mID], new_shares_B[mID], \
+	      mult_batch->SetPreprocessing(new_shares_A[mID], new_shares_B[mID], \
 				     new_shares_A[mID] * new_shares_B[mID] - new_shares_C[mID]);
       }
     }
@@ -84,13 +86,13 @@ namespace dp {
     void Circuit::SetDummyLambdas(FF lambda) {
       // Output wires of multiplications
       for (std::size_t layer = 0; layer < GetDepth(); layer++) {
-	for (auto mult_gate : mFlatMultLayers[layer]) {
-	  mult_gate->SetDummyLambda(lambda);
-	}
+	      for (auto mult_gate : mFlatMultLayers[layer]) {
+	        mult_gate->SetDummyLambda(lambda);
+	      }
       }
       // Output wires of input gates
       for (auto input_gate : mInputGates) {
-	input_gate->SetLambda(lambda);
+	      input_gate->SetLambda(lambda);
       }
     }
 
@@ -98,11 +100,11 @@ namespace dp {
     void Circuit::PopulateDummyLambdas() {
       // Addition gates
       for (auto add_gate : mAddGates) {
-	(void)add_gate->GetDummyLambda();
+	      (void)add_gate->GetDummyLambda();
       }
       // Output gates
       for (auto output_gate : mOutputGates) {
-	(void)output_gate->GetDummyLambda();
+	      (void)output_gate->GetDummyLambda();
       }      
     }
 
@@ -176,43 +178,43 @@ namespace dp {
     }
     void Circuit::PrepMultP1ReceivesAndSends() {
       for (auto mult_layer : mMultLayers) {
-	for (auto mult_batch : mult_layer.mBatches) {
-	  (void) mult_batch;
-	  mCorrelator.PrepMultP1ReceivesAndSends();
-	}
+	      for (auto mult_batch : mult_layer.mBatches) {
+	        (void) mult_batch;
+	        mCorrelator.PrepMultP1ReceivesAndSends();
+	      } 
       }      
     }
     void Circuit::PrepMultPartiesReceive() {
       for (auto mult_layer : mMultLayers) {
-	for (auto mult_batch : mult_layer.mBatches) {
-	  mCorrelator.PrepMultPartiesReceive(mult_batch);
-	}
+	      for (auto mult_batch : mult_layer.mBatches) {
+	        mCorrelator.PrepMultPartiesReceive(mult_batch);
+	      }
       }
     }
 
     void Circuit::PrepIOPartiesSendOwner() {
       for (auto input_layer : mInputLayers) {
-	for (auto input_batch : input_layer.mBatches) {
-	  mCorrelator.PrepInputPartiesSendOwner(input_batch);
-	}
+	      for (auto input_batch : input_layer.mBatches) {
+	        mCorrelator.PrepInputPartiesSendOwner(input_batch);
+	      }
       }
       for (auto output_layer : mOutputLayers) {
-	for (auto output_batch : output_layer.mBatches) {
-	  mCorrelator.PrepOutputPartiesSendOwner(output_batch);
-	}
+	      for (auto output_batch : output_layer.mBatches) {
+	        mCorrelator.PrepOutputPartiesSendOwner(output_batch);
+	      }
       }
     }
 
     void Circuit::PrepIOOwnerReceives() {
       for (auto input_layer : mInputLayers) {
-	for (auto input_batch : input_layer.mBatches) {
-	  mCorrelator.PrepInputOwnerReceives(input_batch);
-	}
+	      for (auto input_batch : input_layer.mBatches) {
+	        mCorrelator.PrepInputOwnerReceives(input_batch);
+	      } 
       }
       for (auto output_layer : mOutputLayers) {
-	for (auto output_batch : output_layer.mBatches) {
-	  mCorrelator.PrepOutputOwnerReceives(output_batch);
-	}
+	      for (auto output_batch : output_layer.mBatches) {
+	        mCorrelator.PrepOutputOwnerReceives(output_batch);
+	      }
       }
     }
   
